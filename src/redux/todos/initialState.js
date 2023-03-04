0f522cbd52622dcb0ee09c7679dcab9f32de4325
mahadidev7 <mahadidev7@gmail.com>
2023-03-04 23:41:00 +0600

    reducer added of todos redux

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const initialState =  [
+    {
+        id: 1,
+        text: "Learn React JS",
+        completed: true,
+    },
+    {
+        id: 2,
+        text: "Learn Redux",
+        completed: false,
+        color: "red"
+    },
+]
+
+export default initialState
