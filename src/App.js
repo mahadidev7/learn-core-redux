e575a03547bebb146f710dac24cb73f527b1e89b
mahadidev7 <mahadidev7@gmail.com>
2023-03-06 19:45:10 +0600

    todos list fetures half add, source: 3.6, time: 11:47

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Provider } from "react-redux";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
+import store from "./redux/store";
+
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
         <Navbar />
 
@@ -24,7 +27,7 @@ function App() {
     <div className="grid place-items-center bg-blue-100 px-6 font-sans">
       <p className="pb-5">Create by Mahadidev7</p>
     </div>
-    </>
+    </Provider>
   );
 }
 
